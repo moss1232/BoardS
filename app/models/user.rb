e0ef91df08bcd70d25759a9e2eae931ee6fcf360
moss1232 <s.mohiro3009@me.com>
2022-01-27 08:54:45 +0900

    new message model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-            # Include default devise modules.
-            devise :database_authenticatable, :registerable,
-                    :recoverable, :rememberable, :validatable
-            include DeviseTokenAuth::Concerns::User
-  has_many :messagesend
+# Include default devise modules.
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+  has_many :messages
 
   validates :name, presence: true
   validates :name, length: {maximum: 30}
