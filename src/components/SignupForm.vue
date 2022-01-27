70ca8d2668ff22ebf611ff3154bb7b1eba189e50
moss1232 <s.mohiro3009@me.com>
2022-01-27 15:33:19 +0900

    finish 6-7

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2>アカウントを登録</h2>
+  <form @submit.prevent="signUp">
+    <input type="text" required placeholder="名前" v-model="name">
+    <input type="email" required placeholder="メールアドレス" v-model="email">
+    <input type="password" required placeholder="パスワード" v-model="password">
+    <input type="password" required placeholder="パスワード（確認用）" v-model="passwordConfirmation">
+    <button>登録する</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: '',
+      email: '',
+      password: '',
+      passwordConfirmation: ''
+    }
+  },
+  methods: {
+    async signUp () {
+      console.log(this.name, this.email, this.password, this.passwordConfirmation)
+    }
+  }
+}
+</script>
