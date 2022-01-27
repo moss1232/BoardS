5cec0f1b8485340d026f4ec1831bca83af848bba
moss1232 <s.mohiro3009@me.com>
2022-01-28 08:40:26 +0900

    new element

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import  './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus).use(router).mount('#app')
