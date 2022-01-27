7ea8d751802037d61303ea9b22c9a27f58f102db
moss1232 <s.mohiro3009@me.com>
2022-01-27 13:56:09 +0900

    finish like

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
   has_many :messages
+  has_many :likes
 
   validates :name, presence: true
   validates :name, length: {maximum: 30}
