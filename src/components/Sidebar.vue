14c5541e7bda18d8a1cb3b1a95b6667c0ef1e4f8
moss1232 <s.mohiro3009@me.com>
2022-01-28 21:07:03 +0900

    finish

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,23 +1,24 @@
 <template>
-  <el-dropdown>
-    <span class="el-dropdown-link">
-      Dropdown List
-      <el-icon class="el-icon--right">
-        <arrow-down />
-      </el-icon>
-    </span>
-    <template #dropdown>
-      <el-dropdown-menu>
-        <el-dropdown-item>Action 1</el-dropdown-item>
-        <el-dropdown-item>Action 2</el-dropdown-item>
-        <el-dropdown-item>Action 3</el-dropdown-item>
-        <el-dropdown-item disabled>Action 4</el-dropdown-item>
-        <el-dropdown-item divided>Action 5</el-dropdown-item>
-      </el-dropdown-menu>
-    </template>
-  </el-dropdown>
+  <p>
+    with extra class <b>is-loading</b>, your icon is able to rotate 360 deg in 2
+    seconds, you can also override this
+  </p>
+  <el-icon :size="20">
+    <edit />
+  </el-icon>
+  <el-icon color="#409EFC" class="no-inherit">
+    <share />
+  </el-icon>
+  <el-icon>
+    <delete />
+  </el-icon>
+  <el-icon class="is-loading">
+    <loading />
+  </el-icon>
+  <el-button type="primary">
+    <el-icon style="vertical-align: middle;">
+      <search />
+    </el-icon>
+    <span style="vertical-align: middle;"> Search </span>
+  </el-button>
 </template>
-
-<script lang="ts" setup>
-import { ArrowDown } from '@element-plus/icons-vue'
-</script>
