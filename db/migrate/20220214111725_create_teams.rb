11834a6bbe0d0456ad916389390e5b24b0f420bd
moss1232 <s.mohiro3009@me.com>
2022-03-02 12:41:13 +0900

    Fix:migration

--- db/migrate/20220214111725_create_teams.rb
+++ db/migrate/20220214111725_create_teams.rb
@@ -1,5 +1,5 @@
-class CreateUserTeamRelationships < ActiveRecord::Migration[6.1]
-  def change
+class CreateTeams < ActiveRecord::Migration[6.1]
+    def change
     create_table :user_team_relationships do |t|
       t.references :user, foreign_key: true
       t.references :team, foreign_key: true
--- db/migrate/20220215001308_create_user_team_relationships.rb
+++ db/migrate/20220215001308_create_user_team_relationships.rb
@@ -1,5 +1,6 @@
-class CreateTeams < ActiveRecord::Migration[6.1]
-  def change
+class CreateUserTeamRelationships < ActiveRecord::Migration[6.1]
+
+    def change
     create_table :teams do |t|
       t.string :name, null: false
       t.string :password, null: false
