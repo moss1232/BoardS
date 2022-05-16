bcd1c0d3e1ebff40698d4f04ce11f7d7090c699a
moss1232 <s.mohiro3009@me.com>
2022-05-16 22:46:27 +0900

    Before:add:nginx

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,12 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins(%r{http://172\.\d{2}\.253\.158:\d{4}},
-            %r{http://localhost:\d{4}},
-            'https://board-vue-frontend.herokuapp.com',
-          'Boards-1134248987.ap-northeast-1.elb.amazonaws.com',
-          'Boards-1134248987.ap-northeast-1.elb.amazonaws.com:80'
-        )
+    origins "*"
+    # origins(%r{http://172\.\d{2}\.253\.158:\d{4}},
+    #         %r{http://localhost:\d{4}},
+    #         'https://board-vue-frontend.herokuapp.com',
+    #       'Boards-1134248987.ap-northeast-1.elb.amazonaws.com',
+    #       'Boards-1134248987.ap-northeast-1.elb.amazonaws.com:80'
+    #     )
     resource '*',
              headers: :any,
              expose: %w[access-token expiry token-type uid client],
