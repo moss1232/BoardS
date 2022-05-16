5082a7a2e3adc8207768978ebde25a4800e3a713
moss1232 <s.mohiro3009@me.com>
2022-05-16 21:17:36 +0900

    Add:bindport
    Fix:routes
    
    Before:add:nginx
    
    Before:nginx

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 set -e
 
-# Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 cd /app
-# RAILS_ENV=production bin/rails db:create
-# RAILS_ENV=production bin/rails db:migrate
-# RAILS_ENV=production bin/rails db:seed
-bundle exec rails s -p 3000
-
-# change userid and groupid for volume premission
-# usermod -u 1002 -o -m tomohiro
-# groupmod -g 1002 tomohiro
+# RAILS_ENV=production bin/rails db:reset
+bundle exec rails s -p 3000 -b '0.0.0.0'
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
