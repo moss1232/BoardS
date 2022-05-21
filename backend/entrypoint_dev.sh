320436069028aaf491dff2b2288c91d326fdb3db
moss1232 <s.mohiro3009@me.com>
2022-05-21 11:41:35 +0900

    Update:.dockerignore

--- backend/entrypoint_dev.sh
+++ backend/entrypoint_dev.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
+# Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 
-rm -f /myapp/tmp/pids/server.pid
-cd /app
+rm -f /app/tmp/pids/server.pid
+# cd /app
 # bin/rails db:reset
-bundle exec rails s -p 3000  -b '0.0.0.0'
-# bundle exec puma -C config/puma.rb
+
+# volumeするためuser,permissionの変更
+echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
+useradd -u $UID -o user
+groupmod -g $GID user
+chown -R user:user /app
+# chown -R user:user /app/Gemfile /Gemfile.lock
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
