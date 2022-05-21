9472f0bed566fde0e223c05fb160ccaef9f99af5
moss1232 <s.mohiro3009@me.com>
2022-05-21 16:04:31 +0900

    Add:wait-for-it

--- backend/entrypoint_dev.sh
+++ backend/entrypoint_dev.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 set -e
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
-
 rm -f /app/tmp/pids/server.pid
-# cd /app
-# bin/rails db:reset
 
 # volumeするためuser,permissionの変更
 echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
 useradd -u $UID -o user
 groupmod -g $GID user
 chown -R user:user /app
-# chown -R user:user /app/Gemfile /Gemfile.lock
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
